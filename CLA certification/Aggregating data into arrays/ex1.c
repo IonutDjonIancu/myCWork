43dbdd9f876f5227a344278f9ff91a986072bd38
iancu ionut <iiancu85@gmail.com>
2019-03-06 20:41:22 +0200

    06.02.2019

---
+++ CLA certification/Aggregating data into arrays/ex1.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+    int main(void) { 
+        int i,j=0; 
+        for(i = 0; i < 10; i+=2) 
+            switch(i) { 
+                case 0: j++; break; 
+                case 2: j++; 
+                case 4: j++; break; 
+                default: j--; 
+            } 
+        printf("%d",j); 
+        return 0;
+    }
---
+++ CLA certification/Aggregating data into arrays/ex5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main(void) { 
+        int i=1,j=2,*p; 
+        p = &i; 
+        *p = j; 
+        *p = 2 * j; 
+        i = *p; 
+        printf("%d",i); 
+        return 0; 
+    }
