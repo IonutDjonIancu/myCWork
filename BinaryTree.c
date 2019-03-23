4ca3afdbacf742b49a3ba4700347421bd2c48bfa
iancu ionut <iiancu85@gmail.com>
2019-03-23 10:43:18 +0200

    23.03.2019

---
+++ BinaryTree.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#pragma warning(disable:4996)
+#pragma warning(disable:4101)              
+
+// binary tree
+
+typedef struct Node {
+	int value;
+	struct Node *leftNext;
+	struct Node *rightNext;
+} Node;
+
+Node *createNode(int data) {
+
+	Node *newNode = (Node *)malloc(sizeof(Node));
+	newNode->value = data;
+
+	newNode->leftNext = NULL;
+	newNode->rightNext = NULL;
+
+	return newNode;
+}
+
+int main(int argc, char *argv[]) {
+
+	Node *root = createNode(1);
+	root->leftNext = createNode(2);
+	root->rightNext = createNode(3);
+
+	root->leftNext->leftNext = createNode(4);
+
+	
+
+
+	_getch();
+	return 0;
+}
